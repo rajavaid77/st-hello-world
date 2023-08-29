55219e67c6677e05586c12c62f0f17438a71ed5b
kayveebee <143530553+kayveebee@users.noreply.github.com>
2023-08-29 11:52:16 -0400

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,2 +1,16 @@
 import streamlit as st
-st.write('Hello World!')
+
+st.title('File Uploader')
+
+# Display a file uploader widget
+uploaded_file = st.file_uploader("Choose a file", type=["csv", "txt", "xlsx"])
+
+if uploaded_file is not None:
+    # You can process the uploaded file here
+    st.write("You selected the following file:")
+    st.write(uploaded_file.name)
+    
+    # For example, you can read and display the contents of the file
+    file_contents = uploaded_file.read()
+    st.write("File Contents:")
+    st.write(file_contents)
